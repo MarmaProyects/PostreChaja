e62435a2d3731c707ee92465176a107eb266b471
axelCoronel <lexel61@gmail.com>
2024-05-06 20:20:43 -0300

    [views_products-show] Vista producto

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -4,39 +4,43 @@
             <section class="product">
                 <div class="product__photo">
                     <div class="photo-container">
-                        <div class="photo-main">
-                            <div class="controls">
-                                <i class="material-icons"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
-                                        <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5" />
-                                    </svg></i>
-                                <i class="material-icons"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                                        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
-                                    </svg></i>
-                            </div>
-                            <div id="main-carousel" class="carousel slide" data-bs-ride="carousel">
-                                <div class="carousel-inner">
-                                    @foreach ($product->images as $index => $image)
-                                    <div id_imagen_carrusel="{{$index}}" class="carousel-item {{$index == 0 ? 'active' : ''}}">
-                                        <img src="data:image/jpg;base64, {{$image->base64}}" class="d-block w-100" alt="green apple slice">
-                                    </div>
-                                    @endforeach
+                        <div class="photo-container-content">
+                            <div class="photo-main">
+                                <div class="controls">
+                                    <i class="material-icons"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
+                                            <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5" />
+                                        </svg></i>
+                                    <i class="material-icons"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                                            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+                                        </svg></i>
                                 </div>
-                                <button class="carousel-control-prev" type="button" data-bs-target="#main-carousel" data-bs-slide="prev">
-                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Previous</span>
-                                </button>
-                                <button class="carousel-control-next" type="button" data-bs-target="#main-carousel" data-bs-slide="next">
-                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Next</span>
-                                </button>
+                                <div class="Carrusel">
+                                    <div id="main-carousel" class="carousel slide" data-bs-ride="carousel">
+                                        <div class="carousel-inner">
+                                            @foreach ($product->images as $index => $image)
+                                            <div id_imagen_carrusel="{{$index}}" class="carousel-item {{$index == 0 ? 'active' : ''}}">
+                                                <img src="data:image/jpg;base64, {{$image->base64}}" class="d-block w-100" alt="green apple slice">
+                                            </div>
+                                            @endforeach
+                                        </div>
+                                        <button class="carousel-control-prev" type="button" data-bs-target="#main-carousel" data-bs-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Previous</span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button" data-bs-target="#main-carousel" data-bs-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Next</span>
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="photo-album">
-                            <ul>
-                                @foreach ($product->images as $index => $image)
-                                <li><button class="album-btn" id_imagen_album="{{$index}}"><img src="data:image/jpg;base64, {{$image->base64}}" alt="alo"></button></li>
-                                @endforeach
-                            </ul>
+                            <div class="photo-album">
+                                <ul>
+                                    @foreach ($product->images as $index => $image)
+                                    <li><button class="album-btn" id_imagen_album="{{$index}}"><img src="data:image/jpg;base64, {{$image->base64}}" alt="alo"></button></li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
