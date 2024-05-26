5ddf68b958aa0cd0116749697762f90031c23150
unknown <lexel61@gmail.com>
2024-05-26 17:08:07 -0300

    [quitar_sistema_roles] Migración para quitar el sistema de roles de spatie Permission

---
+++ database/migrations/2024_05_26_194200_drop_permission_tables.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Obtén los nombres de las tablas desde la configuración
+        $tableNames = config('permission.table_names');
+
+        if (empty($tableNames)) {
+            throw new \Exception('Error: config/permission.php not found and defaults could not be merged. Please publish the package configuration before proceeding, or drop the tables manually.');
+        }
+
+        Schema::dropIfExists($tableNames['role_has_permissions']);
+        Schema::dropIfExists($tableNames['model_has_roles']);
+        Schema::dropIfExists($tableNames['model_has_permissions']);
+        Schema::dropIfExists($tableNames['roles']);
+        Schema::dropIfExists($tableNames['permissions']);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void{}
+};
+
