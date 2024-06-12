da1143f7b93458f0441ec0b3cb2940a2f5865f58
GAguileraMetabix <gaguilerametabix@gmail.com>
2024-06-11 21:47:12 -0300

    [ProfileDesign] diseño

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\Client;
+use App\Models\User;
+use Doctrine\DBAL\Query\QueryException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\Rule;
 use Illuminate\View\View;
 
 class ProfileController extends Controller
@@ -16,25 +21,44 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): View
     {
+        $userId = $request->user()->id;
+        $user = User::findOrFail($userId);
+        $client = Client::findOrFail($user->id);
         return view('profile.edit', [
-            'user' => $request->user(),
-        ]);
+            'user' => $request->user(), 
+        ], compact('client', 'user'));
     }
 
     /**
      * Update the user's profile information.
      */
-    public function update(ProfileUpdateRequest $request): RedirectResponse
+    public function update(ProfileUpdateRequest $request, int $id): RedirectResponse
     {
-        $request->user()->fill($request->validated());
+        $client = Client::findOrFail($id);  
+        $user = $client->user;
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
+        $request->validate([
+            'fullname' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
+            'phone' => ['nullable', 'string', 'max:20'],
+            'address' => ['nullable', 'string', 'max:255'], 
+        ]);
+        try {
+            $userData = $request->only('email');
+            $clientData = $request->only('fullname'); 
+            if ($request->filled('address')) {
+                $clientData['address'] = $request->input('address');
+            }
+            if ($request->filled('phone')) {
+                $clientData['phone'] = $request->input('phone');
+            }
+            $user->update($userData);
+            $client->update($clientData);
+            return redirect()->route('perfil.edit')->with('success', 'Información actualizada!');
+        } catch (QueryException $e) {
+            Log::error('Error Updating category: ' . $e->getMessage());
+            return redirect()->back()->with('error', 'Error al editar!');
         }
-
-        $request->user()->save();
-
-        return Redirect::route('perfil.edit')->with('status', 'perfil-updated');
     }
 
     /**
--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,37 +1,31 @@
-<section>
-    <header>
-        <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Update Password') }}
-        </h2>
-
-        <p class="mt-1 text-sm text-gray-600">
-            {{ __('Ensure your account is using a long, random password to stay secure.') }}
-        </p>
-    </header>
-
+<section>  
     <form method="post" action="{{ route('password.update') }}" class="d-flex">
         @csrf
         @method('put')
-        <div class="justify-content-center">
+        <div class="card-body ">
+            <h3 class="text-uppercase text-red">{{ __('Update Password') }}</h3>
+            <p class=" ">
+                {{ __('Ensure your account is using a long, random password to stay secure.') }}
+            </p>
 
-            <div class="">
-                <x-input-label for="update_password_current_password" :value="__('Current Password')" />
+            <div data-mdb-input-init class="form-outline mb-4">
+                <x-input-label class="form-label" for="update_password_current_password" :value="__('Current Password')" />
                 <x-text-input id="update_password_current_password" name="current_password" type="password"
-                    class="mt-1 block w-full" autocomplete="current-password" />
+                    class="input form-control form-control-lg" autocomplete="current-password" />
                 <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
             </div>
 
-            <div>
-                <x-input-label for="update_password_password" :value="__('New Password')" />
-                <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full"
-                    autocomplete="new-password" />
+            <div data-mdb-input-init class="form-outline mb-4">
+                <x-input-label class="form-label" for="update_password_password" :value="__('New Password')" />
+                <x-text-input id="update_password_password" name="password" type="password"
+                    class="input form-control form-control-lg" autocomplete="new-password" />
                 <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
             </div>
 
-            <div>
-                <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
+            <div data-mdb-input-init class="form-outline mb-4">
+                <x-input-label class="form-label" for="update_password_password_confirmation" :value="__('Confirm Password')" />
                 <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password"
-                    class="mt-1 block w-full" autocomplete="new-password" />
+                    class="input form-control form-control-lg" autocomplete="new-password" />
                 <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
             </div>
 
