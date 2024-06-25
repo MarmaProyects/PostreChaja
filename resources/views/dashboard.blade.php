a9134a492259ebe8a3a7a8cba4949d421cf4d512
leonardoCoronel4 <lexel61@gmail.com>
2024-06-25 16:25:23 -0300

    [Dashboard_add_content] 3 new charts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,33 @@
 <x-auth-layout>
-    <div class="d-flex dashboard">
-        <div class="contenedor">
+    <div class="row dashboard">
+        <div class="contenedor col-6">
             <h2>Productos más visitados</h2>
             <div class="chart-container">
                 <canvas id="mostVisitedProductsChart"></canvas>
             </div>
         </div>
-        <div class="contenedor">
+        <div class="contenedor col-6">
             <h2>Productos por Categoría</h2>
             <div class="chart-container">
                 <canvas id="productsByCategoryChart" style="display:flex; text-align:center"></canvas>
+            </div>
+        </div>
+        <div class="contenedor col-6">
+            <h2>Productos favoritos de los clientes</h2>
+            <div class="chart-container">
+                <canvas id="favoriteChart" style="display:flex; text-align:center"></canvas>
+            </div>
+        </div>
+        <div class="contenedor col-6">
+            <h2>Compras Mensuales</h2>
+            <div class="chart-container">
+                <canvas id="monthlyPurchasesChart"></canvas>
+            </div>
+        </div>
+        <div class="contenedor col-6">
+            <h2>Productos más vendidos</h2>
+            <div class="chart-container">
+                <canvas id="mostSoldProductsChart"></canvas>
             </div>
         </div>
     </div>
@@ -37,6 +55,7 @@
                     }
                 }
             });
+
             const ctx2 = document.getElementById('productsByCategoryChart').getContext('2d');
             const chart2 = new Chart(ctx2, {
                 type: 'pie',
@@ -73,6 +92,123 @@
                         title: {
                             display: true,
                             text: 'Productos por Categoría'
+                        }
+                    }
+                }
+            });
+
+            const ctx3 = document.getElementById('favoriteChart').getContext('2d');
+            const chart3 = new Chart(ctx3, {
+                type: 'bar',
+                data: {
+                    labels: {!! json_encode($products_favorites->pluck('name')) !!},
+                    datasets: [{
+                        label: 'Cantidad de Favoritos',
+                        data: {!! json_encode($products_favorites->pluck('favorites_count')) !!},
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.4)',
+                            'rgba(54, 162, 235, 0.4)',
+                            'rgba(255, 206, 86, 0.4)',
+                            'rgba(75, 192, 192, 0.4)',
+                            'rgba(153, 102, 255, 0.4)',
+                            'rgba(255, 159, 64, 0.4)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: 'top',
+                        },
+                        title: {
+                            display: true,
+                            text: 'Productos Favoritos'
+                        }
+                    }
+                }
+            });
+
+            const ctx4 = document.getElementById('monthlyPurchasesChart').getContext('2d');
+            const monthlyPurchasesChart = new Chart(ctx4, {
+                type: 'bar',
+                data: {
+                    labels: {!! json_encode($monthly_data->pluck('month')) !!},
+                    datasets: [{
+                        label: 'Cantidad de Compras',
+                        data: {!! json_encode($monthly_data->pluck('total_purchases')) !!},
+                        backgroundColor: 'rgba(75, 192, 192, 0.4)',
+                        borderColor: 'rgba(75, 192, 192, 1)',
+                        borderWidth: 1
+                    }, {
+                        label: 'Ganancias Totales',
+                        data: {!! json_encode($monthly_data->pluck('total_revenue')) !!},
+                        backgroundColor: 'rgba(255, 99, 132, 0.4)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: 'top',
+                        },
+                        title: {
+                            display: true,
+                            text: 'Compras Mensuales y Ganancias'
+                        }
+                    },
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            ticks: {
+                                callback: function(value, index, values) {
+                                    // Formatear el valor con "$" delante
+                                    return '$' + value.toFixed(2);
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            const ctx5 = document.getElementById('mostSoldProductsChart').getContext('2d');
+            const mostSoldProductsChart = new Chart(ctx5, {
+                type: 'bar',
+                data: {
+                    labels: {!! json_encode($products_sold->pluck('name')) !!},
+                    datasets: [{
+                        label: 'Cantidad Vendida',
+                        data: {!! json_encode($products_sold->pluck('total_sold')) !!},
+                        backgroundColor: 'rgba(54, 162, 235, 0.4)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: 'top',
+                        },
+                        title: {
+                            display: true,
+                            text: 'Productos Más Vendidos'
+                        }
+                    },
+                    scales: {
+                        y: {
+                            beginAtZero: true
                         }
                     }
                 }
