a1927fc1d993e18169a1112db05e2598b37ce5b5
GAguileraMetabix <gaguilerametabix@gmail.com>
2024-08-13 14:26:55 -0300

    [DeployCpanel] prueba

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
@@ -13,5 +14,13 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
